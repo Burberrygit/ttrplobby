1015f7231506dc99efc26a13b2f867f300c87e82
Mr.Burberry <wannamakeralain@gmail.com>
2025-10-02 11:28:16 -0400

    Update next-sitemap.config.js

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,15 +1,18 @@
-// frontend/next-sitemap.config.js
 const path = require('path');
 
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.ttrplobby.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-
-  // Absolute paths remove any CWD/monorepo quirks
   outDir: path.resolve(__dirname, 'public'),
   buildManifestFile: path.resolve(__dirname, '.next', 'build-manifest.json'),
 
   exclude: ['/api/*'],
-};
+
+  // NEW: point robots.txt at your dynamic sitemap too
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.ttrplobby.com/server-sitemap.xml',
+    ],
+  },
+}
