e4c34a04a026a237423c9e2a0f2537e80caf5f22
Mr.Burberry <wannamakeralain@gmail.com>
2025-10-07 12:37:45 -0400

    Add files via upload

---
+++ frontend/src/app/auth/callback/page - 2025-10-07T121649.153.tsx
@@ -0,0 +1,8 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+import CallbackClient from './CallbackClient'
+
+export default function Page() {
+  return <CallbackClient />
+}
